7092614a992c9f62ae490004efc11e2eba604a55
Alex <alchkim@umich.edu>
2014-10-26 12:58:26 -0400

    Upload Project

---
+++ Project3/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 curLoc;
+	private GameObject prevLoc;
+	public float moveSpeed = 2;
+	private bool set;
+	public GameObject marker;
+
+	void Update () 
+	{
+		InputListen 	();
+		Poof ();
+	}
+	
+	private void InputListen() {
+		curLoc = transform.position;
+		
+		if(Input.GetKey(KeyCode.A))
+			curLoc.x -= moveSpeed * Time.fixedDeltaTime;
+		if(Input.GetKey(KeyCode.D))
+			curLoc.x += moveSpeed * Time.fixedDeltaTime;
+		if(Input.GetKey(KeyCode.W))
+			curLoc.z += moveSpeed * Time.fixedDeltaTime;
+		if(Input.GetKey(KeyCode.S))
+			curLoc.z -= moveSpeed * Time.fixedDeltaTime;
+		if(Input.GetKey(KeyCode.Space))
+			curLoc.y += 2* moveSpeed * Time.fixedDeltaTime;
+		transform.position = curLoc;
+		
+	}
+
+	private void Poof() {
+		if(Input.GetKeyUp(KeyCode.J)) {
+			if(set) {
+				Destroy(prevLoc);
+				transform.position = prevLoc.transform.position;
+				set = false;
+			} else {
+				prevLoc = Instantiate(marker, curLoc, Quaternion.identity) as GameObject;
+				set = true;
+			}
+		}
+	}
+}
