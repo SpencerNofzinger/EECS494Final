9fa9255b89cc82f2ad002c55a342a1fb8f841e7a
Alex <alchkim@umich.edu>
2014-10-27 14:40:34 -0400

    Alex's Level

---
+++ Project3/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	public GameObject spawnObject;
+	float spawnTime;
+
+	void Start () {
+		spawnTime = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (spawnTime > 5.0f) {
+			Instantiate (spawnObject, transform.position, Quaternion.identity);
+			spawnTime = 0.0f;
+		}
+		spawnTime += Time.deltaTime;
+	}
+}
