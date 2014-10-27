67f5cfdea996019e9b5af070cf1a18b358018dd3
pxbhuang <pxbhuang@gmail.com>
2014-10-27 14:54:37 -0400

    Patrick level

---
+++ Project3/Assets/BridgeSwitch_Patrick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeSwitch_Patrick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.CompareTag ("Player"))
+		{
+			GameObject[] BridgeList = GameObject.FindGameObjectsWithTag ("Bridge");
+			for(int i = 0; i < BridgeList.Length; ++i)
+			{	
+				if (BridgeList[i].GetComponent<Bridge_Patrick>().rotation < 4)
+					BridgeList[i].GetComponent<Bridge_Patrick>().rotation++;
+				else
+					BridgeList[i].GetComponent<Bridge_Patrick>().rotation = 1;
+			}
+		}
+	}
+}
