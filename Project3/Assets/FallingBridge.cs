b8d776c77a845edc55857c93315052308d4ad14a
Kyle Zhu <kylezh@umich.edu>
2014-10-29 13:26:40 -0400

    Worked on my level
    My level uses falling bridges

---
+++ Project3/Assets/FallingBridge.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingBridge : MonoBehaviour {
+
+	public Vector3 velocity = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += velocity * Time.deltaTime;
+	}
+	void OnCollisionExit(Collision col){
+		if(col.collider.gameObject.CompareTag("Player")){
+			velocity.y = -9.8f;
+		}
+			
+	}
+}
