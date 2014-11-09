70762f4175fb0a69678706c915e53dd20281c9c8
Alex <alchkim@umich.edu>
2014-11-09 16:44:56 -0500

    Instructions done

---
+++ Project3/Assets/Scripts/InstructionList.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionList : MonoBehaviour {
+	int inUse;
+	GUIText[] list;
+	bool showGUI;
+
+	void Start () {
+		list = this.GetComponentsInChildren<GUIText> ();
+		for (int i = 0; i < list.Length; i++) {
+			list[i].transform.position = new Vector3(0f, 1f, 0f);
+			list[i].enabled = false;
+		}
+		inUse = -1;
+	}
+
+	public void Enter (int i) {
+		Debug.Log ("inUse: " + inUse);
+		if (inUse == -1) {
+//			list[i].enabled = true;
+			showGUI = true;
+			inUse = i;
+		} else {
+//			list[inUse].enabled = false;
+//			list[i].enabled = true;
+			showGUI = true;
+			inUse = i;
+		}
+	}
+
+	public void Exit (int i) {
+		Debug.Log ("inUse: " + inUse);
+//		list[i].enabled = false;
+		showGUI = false;
+		inUse = -1;
+	}
+
+	void OnGUI() {
+		if(showGUI) {
+			int width = 200, height = 50;
+			GUI.Box(new Rect((Screen.width - width)/2, 0, width, height), list[inUse].text);
+//			        ToString ());
+		}
+	}
+}
