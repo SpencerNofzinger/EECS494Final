8227f19d47354ae6dab263dfcabb4adaddc50d3b
snofz <snofz@umich.edu>
2014-11-09 17:46:10 -0500

    New Level
    Added a moderately difficult level with moving platforms. Changed moving
    platform script so if manual checked, only moves when switch touched

--- Project3/Assets/Scripts/LoopingMovingPlatform.cs
+++ Project3/Assets/Scripts/LoopingMovingPlatform.cs
@@ -2,46 +2,79 @@
 using System.Collections;
 
 public class LoopingMovingPlatform : MonoBehaviour {
+	public bool manualSwitch = false;
 	public Vector3 velocity;
 	public float length;
 	Vector3 startPos;
 	bool forward = true;
 	bool paused = false;
+	bool on = false;
 	float currentPauseTime = 0;
 	public float pauseTime = 0;
 
 	// Use this for initialization
 	void Start () {
 		startPos = transform.position;
+		if(manualSwitch)
+		{
+			velocity = - velocity;
+			forward = !forward;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!paused){
-			Vector3 pos = transform.position;
-			pos += velocity * Time.deltaTime;
-			transform.position = pos;
-			if(forward && Vector3.Distance (transform.position, startPos) > length)
-			{
-				velocity = -velocity;
-				forward = false;
-				paused = true;
+		if(!manualSwitch){
+			if(!paused){
+				Vector3 pos = transform.position;
+				pos += velocity * Time.deltaTime;
+				transform.position = pos;
+				if(forward && Vector3.Distance (transform.position, startPos) > length)
+				{
+					velocity = -velocity;
+					forward = false;
+					paused = true;
+				}
+				if(!forward && Vector3.Distance (transform.position, startPos) < .1f)
+				{
+					velocity = -velocity;
+					transform.position = startPos;
+					forward = true;
+					paused = true;
+				}
 			}
-			if(!forward && Vector3.Distance (transform.position, startPos) < .1f)
-			{
-				velocity = -velocity;
-				transform.position = startPos;
-				forward = true;
-				paused = true;
+			else{
+				currentPauseTime += Time.deltaTime;
+				if(currentPauseTime > pauseTime)
+				{
+					currentPauseTime = 0;
+					paused = false;
+				}
 			}
 		}
 		else{
-			currentPauseTime += Time.deltaTime;
-			if(currentPauseTime > pauseTime)
+			if(on)
 			{
-				currentPauseTime = 0;
-				paused = false;
+				Vector3 pos = transform.position;
+				pos += velocity * Time.deltaTime;
+				transform.position = pos;
+				if(forward && Vector3.Distance (transform.position, startPos) > length)
+				{
+					on = false;
+				}
+				if(!forward && Vector3.Distance (transform.position, startPos) < .1f)
+				{
+					transform.position = startPos;
+					on = false;
+				}
 			}
 		}
+	}
+
+	public void switchOn()
+	{
+		on = true;
+		velocity = -velocity;
+		forward = !forward;
 	}
 }
---
+++ Project3/Assets/Scripts/MovingPlatformSwitch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformSwitch : MonoBehaviour {
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.CompareTag ("Player"))
+		{
+			GameObject[] PlatList = GameObject.FindGameObjectsWithTag ("MovingPlatform");
+			for(int i = 0; i < PlatList.Length; ++i)
+			{
+				PlatList[i].GetComponent<LoopingMovingPlatform>().switchOn();
+			}
+		}
+	}
+}
