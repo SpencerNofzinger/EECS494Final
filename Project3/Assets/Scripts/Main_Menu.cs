cca67a20bbcf5500e5528795c4cbbcf093c7cb35
Alex <alchkim@umich.edu>
2014-12-07 18:27:35 -0500

    Main menu added

---
+++ Project3/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour {
+//	public Texture btnTexture;
+	public string first_scene;
+	public string scene_select;
+	float mid_width;
+	float mid_height;
+	float start_width = 100;
+	float start_height = 50;
+	float select_width = 100;
+	float select_height = 50;
+
+	void Start () {
+		mid_width = Screen.width / 2;
+		mid_height = Screen.height / 2;
+	}
+
+	void OnGUI () {
+//		if (!btnTexture) {
+//			Debug.LogError("Please assign a texture on the inspector");
+//			return;
+//		}
+		if (GUI.Button(new Rect(mid_width - start_width / 2, mid_height - start_height / 2, start_width, start_height), "Start")) {
+//			Debug.Log("Clicked the button with an image");
+			Application.LoadLevel (first_scene);
+		}
+
+		if (GUI.Button(new Rect(mid_width - select_width / 2, mid_height + select_height / 2 + start_height, select_width, select_height), "Stage Select")) {
+//			Debug.Log("Clicked the button with text");
+			Application.LoadLevel (scene_select);
+		}
+	}
+}
---
+++ Project3/Assets/Scripts/OnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+	public string nextScene;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
