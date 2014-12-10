b8db42d826013259fa9d0f7e4c038079f28ba6f6
snofz <snofz@umich.edu>
2014-12-09 19:25:24 -0500

    Fixed Kill Blanket, small bug

--- Project3/Assets/Scripts/Kill.cs
+++ Project3/Assets/Scripts/Kill.cs
@@ -6,7 +6,7 @@ public class Kill : MonoBehaviour {
 	void OnTriggerEnter(Collider col){
 		if(col.gameObject.CompareTag ("Player"))
 		{
-			col.GetComponent<Player>().reload ();
+			col.GetComponent<Player2>().reload ();
 		}
 		if(col.gameObject.CompareTag ("Marker"))
 		{
